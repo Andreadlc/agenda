827b16d99b9d863b3f2d72dbff84e2265869c611
Andreadlc <andreadelcros7@gmail.com>
2025-02-11 20:17:05 +0100

    creation des test pour planning collection

---
+++ projet/src/test/java/PlanningCollectionsTest.java
@@ -0,0 +1,35 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import modele.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.TreeSet;
+
+class PlanningCollectionsTest {
+    private PlanningCollections planning;
+
+    @BeforeEach
+    void setUp() {
+        planning = new PlanningCollections();
+    }
+
+    @Test
+    void testAjoutReservation() {
+        // À compléter après
+    }
+
+    @Test
+    void testGetReservationsParDate() {
+        // À compléter après
+    }
+
+    @Test
+    void testGetReservationsParIntitule() {
+        // À compléter après
+    }
+
+    @Test
+    void testGetReservationsParSemaine() {
+        // À compléter après
+    }
+}
---
+++ projet/src/test/java/test.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class test {
+
+
+    @Test
+    public void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result, "L'addition de 2 et 3 doit donner 5");
+    }
+
+}
